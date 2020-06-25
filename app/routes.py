ce96cf8db109a33f6d529a6a67507684372bfe3a
Abhishekguragol <abhishekguragol@gmail.com>
2020-06-25 22:08:16 +0530

    Updated the code for registering

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from app import app
 from app.models import User
 from app import db
-from flask import request, jsonify, make_response
+from flask import request, jsonify, make_response, render_template, redirect,url_for
 from werkzeug.security import generate_password_hash, check_password_hash
 import uuid
 import jwt
@@ -12,12 +12,9 @@ from functools import wraps
 @app.route('/')
 @app.route('/index')
 def index():
-    return "HCL Hackathon!"
+    return "<h1> Welcome </h1>"
 
-#@app.route('/login')
-#deflogin():
-    #form = LoginForm()
-    #return render_template('login.html', title='Sign In', form=form)
+
 
 def token_required(f):
     @wraps(f)
@@ -45,37 +42,47 @@ def token_required(f):
 # Sing Up route for frontend request, response in json
 @app.route('/register', methods=['GET', 'POST'])
 def signup_user():
-    data = request.get_json()
+    print("In register")
+    if request.method == 'POST':
+        print("Register Sub")
+        data = request.get_json()
+        print(request.form)
 
-    hashed_password = generate_password_hash(data['password'], method='sha256')
+        hashed_password = generate_password_hash(request.form['password'], method='sha256')
 
-    new_user = User(public_id=str(uuid.uuid4()), name=data['name'], email=data['email'], username=data['username'], password=hashed_password)
+        new_user = User(public_id=str(uuid.uuid4()), name=request.form['name'], email=request.form['email'], username=request.form['username'], password=hashed_password)
 
-    try:
-        db.session.add(new_user)
-        db.session.commit()
-    except:
-        return jsonify({'message': 'Unsuccessful registration'})
+        try:
+            
+            db.session.add(new_user)
+            
+            db.session.commit()
+            
+            return redirect(url_for('index'))
 
-    return jsonify({'message': 'registered successfully'})
+        except:
+            return jsonify({'message': 'Unsuccessful registration'})
+
+    return render_template('signup.html', title='Sign Up')
 
 
 # Login route for frontend request, response in json
 @app.route('/login', methods=['GET', 'POST'])
 def login_user():
-    auth = request.authorization
+    if request.method == 'POST':
+        #auth = request.authorization
 
-    if not auth or not auth.username or not auth.password:
-        return make_response('could not verify', 401, {'WWW.Authentication': 'Basic realm: "login required"'})
+        if not request.form["username"] or not request.form["password"]:
+            return make_response('could not verify', 401, {'WWW.Authentication': 'Basic realm: "login required"'})
 
-    user = User.query.filter_by(username=auth.username).first()
+        user = User.query.filter_by(username=request.form["username"]).first()
 
-    if check_password_hash(user.password, auth.password):
-        token = jwt.encode({'public_id': user.public_id, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'])
-        return jsonify({'token' : token.decode('UTF-8')})
-
-    return make_response('could not verify',  401, {'WWW.Authentication': 'Basic realm: "login required"'})
-
+        if check_password_hash(user.password, request.form["password"]):
+            token = jwt.encode({'public_id': user.public_id, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'])
+            return render_template('home.html', title='Sign Up')
+        else:
+            return make_response('could not verify',  401, {'WWW.Authentication': 'Basic realm: "login required"'})
+    return render_template('login.html', title='Sign Up')
 
 # To list all users (use for testing)
 @app.route('/all_users', methods=['GET'])
